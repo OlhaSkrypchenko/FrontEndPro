c1a3099b184676506bd1cc52b4f8d3147fb3a2db
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2020-02-07 17:24:15 +0200

    final chanches

--- HW2020-02-06/script.js
+++ HW2020-02-06/script.js
@@ -5,61 +5,62 @@ let scissors = options[1];
 let paper = options[2];
 
 for (let i = 0; i < 1; i++) {
-    let confirmation;
+  let confirmation;
 
-    let userStep = prompt('Your turn! Stone, scissors or paper?', '');
+  let randomInteger = Math.floor(Math.random() * options.length);
+  let computerStep = options[randomInteger];
 
-    if (typeof userStep === 'string') {
-        userStep = userStep.toLowerCase();
-    } else {
-        confirmation = confirm(`Hmmm...You didn't enter anything...
-                                \nDo you really wanna to continue the game?`);
-        if (confirmation) {
-            i--;
-            continue;
-        } else {
-            break;
-        }
-    }
+  let userStep = prompt('Your turn! Stone, scissors or paper?', '');
 
-    if (!options.includes(userStep)) {
-        confirmation = confirm(`What did you write? You have just three options:
-                                \nstone, scissors or paper! 
-                                \nDo you wanna to continue the game?`);
-        if (confirmation) {
-            i--;
-            continue;
-        } else {
-            break;
-        }
-    }
-    
-    let randomInteger = Math.floor(Math.random() * options.length);
-    let computerStep = options[randomInteger];
-
-    let draw = (userStep === stone && computerStep === stone) 
-                || (userStep === scissors && computerStep === scissors)
-                || (userStep === paper && computerStep === paper);
-    
-    let win = (userStep === stone && computerStep === scissors) 
-                || (userStep === scissors && computerStep === paper)
-                || (userStep === paper && computerStep === stone);
-
-    if (win) {
-        confirmation = confirm(`Your step was ${userStep}, computer step was ${computerStep}.
-                                \nYou win!!!
-                                \nDo you wanna to play the game again?`);
-    } else if (draw) {
-        confirmation = confirm(`Your step was ${userStep}, computer step was ${computerStep}.
-                                \nDraw!!!
-                                \nDo you wanna to play the game again?`);
-    } else {
-        confirmation = confirm(`Your step was ${userStep}, computer step was ${computerStep}.
-                                \nYou loose ((
-                                \nDo you wanna to play the game again?`);
-    }
-
+  if (typeof userStep === 'string') {
+    userStep = userStep.toLowerCase();
+  } else {
+    confirmation = confirm(`Hmmm...You didn't enter anything...
+                            \nDo you really wanna to continue the game?`);
     if (confirmation) {
         i--;
+        continue;
+    } else {
+        break;
     }
+	}
+	
+  if (!options.includes(userStep)) {
+    confirmation = confirm(`What did you write? You have just three options:
+                            \nstone, scissors or paper! 
+                            \nDo you wanna to continue the game?`);
+    if (confirmation) {
+        i--;
+        continue;
+    } else {
+        break;
+    }
+  }
+  
+  let win = (userStep === stone && computerStep === scissors) 
+            || (userStep === scissors && computerStep === paper)
+            || (userStep === paper && computerStep === stone);
+  
+	let draw = userStep === computerStep;
+	
+  switch(true) {
+    case win: 
+      confirmation = confirm(`Your step was ${userStep}, computer step was ${computerStep}.
+                              \nYou win!!!
+                              \nDo you wanna to play the game again?`);
+      break;
+    case draw:
+      confirmation = confirm(`Your step was ${userStep}, computer step was ${computerStep}.
+                              \nDraw!!!
+                              \nDo you wanna to play the game again?`);
+      break;
+    default:
+      confirmation = confirm(`Your step was ${userStep}, computer step was ${computerStep}.
+                              \nYou loose ((
+                              \nDo you wanna to play the game again?`);
+  }
+
+  if (confirmation) {
+    i--;
+  }
 }
