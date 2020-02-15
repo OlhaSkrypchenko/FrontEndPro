4d4b9495eba3297a87e52760c1a7290bf3cbf46e
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2020-02-15 11:25:22 +0200

    removed first version

--- HW2020-02-13/script.js
+++ HW2020-02-13/script.js
@@ -1,22 +1,6 @@
 let arr1 = [25, 'string-string', 13, null, 100, [12, 45, 13, 'skdl', 990], 'next', 'new'];
 let arr2 = ['next', 'string-string', 25, 'new', 13,  89, 100, null];
 
-let result1 = [];
-
-for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-        if (arr1[i] === arr2[j] && typeof arr2[j] === 'number' && arr2[j] % 5 === 0) {
-            result1.push('FIVE');
-        } else if (arr1[i] === arr2[j] && typeof arr2[j] === 'string' && arr2[j].length > 5) {
-            result1.push('FSTR');
-        } else if (arr1[i] === arr2[j]) {
-            result1.push(arr2[j]);
-        }
-    }
-}
-
-console.log(result1);
-
 function compareArr(arr1, arr2) {
     let result = [];
     arr1.forEach(item1 => arr2.forEach(item2 => {
