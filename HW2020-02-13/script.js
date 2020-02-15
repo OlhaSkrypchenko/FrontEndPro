a98f5e674eae79eb011485157ba13f495ffaa147
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2020-02-15 11:31:28 +0200

    some changes

--- HW2020-02-13/script.js
+++ HW2020-02-13/script.js
@@ -3,6 +3,7 @@ let arr2 = ['next', 'string-string', 25, 'new', 13,  89, 100, null];
 
 function compareArr(arr1, arr2) {
     let result = [];
+    
     arr1.forEach(item1 => arr2.forEach(item2 => {
         let isEqualValues = item1 === item2;
         let isNumberModFive = isEqualValues && typeof item1 === 'number' && item1 % 5 === 0;
